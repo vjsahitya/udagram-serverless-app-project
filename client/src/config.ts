89dee2ee662d84e6676a29169c40234e943d4d1f
Sahitya Nigam <32236962+vjsahitya@users.noreply.github.com>
2020-07-14 06:13:27 +0530

    Add files via upload

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+
+const apiId = '3vg0jhaie6'
+
+export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-1-8vpy3w.us.auth0.com',           
+  clientId: 'aBnqLom3wsqBh1u6ilMlMc54GyuPYlGp',         
+  callbackUrl: 'http://localhost:3000/callback'
+}
